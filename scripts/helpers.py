ca949e8107cd15ae649409580d57c12dce7dbf38
Mark Wagner <you@example.com>
2024-12-30 00:23:17 -0500

    Simplified the scripts a little

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -41,3 +41,66 @@ def update_submodules(repos):
                   "git submodule init;" + \
                   "git submodule update --remote;"
             os.system(cmd)
+
+
+def get_data(project_type):
+    """
+    :param project_type: The type of project being built.
+    :return: A Python dictionary with information needed to build a new project.
+    """
+    parent_directory = ''
+    templates_path = ''
+    directory_names = ''
+    project_cc_content = ''
+
+    if project_type == 'd':
+        project_type = 'Desktop'
+        parent_directory = '/prj_desktop/'
+        templates_path = '/templates/desktop'
+        directory_names = ['cmake', 'docs', 'libs', 'python', 'resources', 'scripts', 'src', 'tests']
+        project_cc_content = ''
+
+    elif project_type == 'e':
+        project_type = 'Embedded'
+        parent_directory = '/prj_embedded/'
+        templates_path = '/templates/embedded'
+        directory_names = ['bootloader', 'cmake', 'docs', 'drivers', 'dsp', 'hardware_design', 'libs', 'python',
+                           'resources', 'scripts', 'tests']
+        project_cc_content = '#include <cstdio>' + '\n' + \
+                             'extern "C" {' + '\n' + \
+                             '#include "drivers/driver.h"' + '\n' + \
+                             '}' + '\n' + \
+                             '\n' + \
+                             'int main()' + '\n' + \
+                             '{' + '\n' + \
+                             '    int var = driver();' + '\n' + \
+                             '    printf(\"Hello PROJECTNAME %d \\n\", var);' + '\n' + \
+                             '    return 0;' + '\n' + \
+                             '}' + '\n'
+
+    elif project_type == 'l':
+        project_type ='Library'
+        parent_directory = '/prj_library/',
+        templates_path = '/templates/library'
+        directory_names = ['cmake', 'docs', 'libs', 'python', 'resources', 'scripts', 'src', 'tests']
+        project_cc_content = ''
+
+    elif project_type == 'p':
+        project_type = 'Plugin'
+        parent_directory = '/prj_plugin/'
+        templates_path = '/templates/plugin'
+        directory_names = ['cmake', 'docs', 'dsp', 'libs', 'python', 'resources', 'scripts', 'src', 'tests']
+        project_cc_content = ''
+
+    else:
+        print("Error in get_data.")
+
+    data = {
+        'project_type': project_type,
+        'parent_directory': parent_directory,
+        'templates_path': templates_path,
+        'directory_names': directory_names,
+        'project_cc_content': project_cc_content,
+    }
+
+    return data
---
+++ scripts/update_templates.py
@@ -0,0 +1,8 @@
+
+
+
+
+# https://github.com/MyPublicGitHubAcct/CMakeCatch2/tree/5d71564c122f8e42af8135b6c57a19c3e3bb409b/templates
+
+
+
